e357e84fcc8aa0cfe8dce12a336e86a24d1e9dd0
parinitavishwakarma416@gmail.com <parinitavishwakarma416@gmail.com>
2026-02-22 12:13:53 +0530

    add function

--- script.js
+++ script.js
@@ -206,3 +206,204 @@ function checkAge(age) {
  return "Allowed";
 }
 console.log(checkAge(15));
+
+
+
+// LOOPS -> repeat karna
+//for,while, foreach
+
+// for loop->> kaha se jana hai->kaha tak jana hai->kaise jana hai
+// for(start;end;change)
+for(let i=1;i<101;i++){
+    console.log(i);
+}
+
+/* while->> kaha se jana hai->kab rukna hai -> kaise jana hai
+while(end){
+    code
+    change
+}
+*/
+let p=1;
+while(p<32){
+    console.log(p);
+    p++;
+}
+
+/* do while
+start
+do{
+    code;
+}
+while(end)
+*/
+let q=1;
+do{
+    console.log(q);
+    q++;
+}
+while(q<20);
+
+// break
+//1-20 tak print karao aur agar 13 mile to wahi ruk jao
+for(l=1;l<21;l++){
+    console.log(l);
+    if(l===13){
+        break;
+    }
+}
+
+// continue
+//1-20 tak print ho  magar 13 na print ho 
+for(l=1;l<21;l++){
+    if(l===13){
+        continue;
+    }
+        console.log(l);
+}
+
+
+// FUNCTION -> reuse of code
+
+// function declaration
+function newYear(){
+    console.log("Happy New Year");
+}
+newYear()
+
+ // function expression
+let=function(){   
+    console.log("Happy New Year");
+}
+newYear()
+
+// fat arrow function
+let func=()=>{
+    console.log("happy new year");
+}
+func();
+
+// parameter vs argument
+function dance(v1){
+    console.log(`${v1} naach rha hai`);
+}
+dance("kutta");
+dance("billi");
+dance("bakri");
+dance("ghoda");
+dance("chuha");
+
+function add(v1,v2){        // parameter
+    console.log(v1+v2);
+}
+add(4,5);                   // argument
+add(8,6);
+
+
+//default farameters
+function add(v1=0,v2=3){
+    console.log(v1+v2);
+}
+add();
+
+
+// rest-> jab argument kai sare ho to hame utne ki paramrter banane padte hai,
+// isase bachne ke liye ham rest ka use karte hai
+// agar... function ke parameter space me lage to wo rest operator hai
+// agr wo array annnnnd objects me lage to to wo spread operator ahi
+function abcd(a,b,c, ...val){
+    console.log(a,b,c,val);
+}
+abcd(1,5,3,6,7,1,9,2,);
+
+
+// return
+function abcd(){
+    return 12;
+}
+let val=abcd();
+console.log(val);
+
+
+//first class function -> function ko values ke tarh treat kar sakte hai
+// jaha jaha value ja saktti hai waha waha function bhi ja sakta hai
+function abc(val){
+    val();
+}
+abc(function(){
+    console.log("heyyyy");
+});
+
+
+// higher order funvtion -> wo function jo return kare ek function ya phir accept kare ek function apne parameter me
+// accept
+function abcd(val){
+
+}
+abcd(function(){
+
+});
+
+// return
+function abcd(){
+    return function() {
+        console.log("heeeeyyyyyy");
+    }
+}
+abcd()();
+
+/*
+
+// pure vs impure function  
+// pure function -> jo bahar ki value ko na badle
+// impure function -> jo bahar ki value ko badal de
+let ab=12;
+function abcd(){
+    console.log("pure function")
+}
+
+let abcdef=12;
+function abcde(){
+    abcdef ++;
+}
+
+// closure -> ek function jo return kare ek aur function aur return hone wala function hamesha use karega parent function ka koi variable
+
+function abcd(){
+    let a=12;
+    return function(){
+        console.log(a);
+    }
+}
+
+// lexical function -> function ke andar ka bana hua bas function ke andar use kar sakte hai
+function abcd(){
+    let a=12;
+    function defg(){
+        let b=13;
+        function ghij(){
+            let c=14;
+        }
+    }
+}
+
+*/
+
+// IIFE (Immediately Invoked Function Expression)
+// 
+(function(){
+    console.log("hiii");
+})();
+
+//hoisting -> function banne se ko call karna 
+// function declaration me hoisting hota hai, function expression me nhi kar sakte hai hoisting
+xyz();
+
+
+function xyz(){
+    console.log("heheheheh");
+}
+
+
+
+
